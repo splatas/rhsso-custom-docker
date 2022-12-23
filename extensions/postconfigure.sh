0b8c37d6b0a3ed0f259bfce416fe3b311eb1b4d4
Sergio Platas <splatas@gmail.com>
2022-12-23 12:47:15 +0100

    Update postconfigure.sh

--- extensions/postconfigure.sh
+++ extensions/postconfigure.sh
@@ -6,11 +6,11 @@ $JBOSS_HOME/bin/jboss-cli.sh --file=/opt/eap/extensions/actions.cli
 
 #echo " Running kcadm..."
 # Keycloak ADMIN:
-# kcadm.sh config credentials --server http://localhost:8080/auth --realm giss-certificados --user admin --password CHANGE_ME 
-# kcadm.sh create components -r giss-certificados -s name=rsa-giss -s providerId=rsa -s providerType=org.keycloak.keys.KeyProvider -s 'config.privateKey=["/home/splatas/repo/giss/segunda-implementacion/es-giss-docker/certs/privateKey.pem"]’ -s ‘config.certificate=["/home/splatas/repo/giss/segunda-implementacion/es-giss-docker/certs/caKey.pem"]'
+# kcadm.sh config credentials --server http://localhost:8080/auth --realm $MY_REALM_NAME --user $MI_USER --password CHANGE_ME 
+# kcadm.sh create components -r $REALM_NAME -s name=rsa-giss -s providerId=rsa -s providerType=org.keycloak.keys.KeyProvider -s 'config.privateKey=["/home/splatas/repo/$ALGUNA_CARPETA/segunda-implementacion/$REPOSITORIO/certs/privateKey.pem"]’ -s ‘config.certificate=["/home/splatas/repo/$UN_REPO/segunda-implementacion/$UNA_CARPETA/certs/caKey.pem"]'
 ## --- ##
 #export REALM_NAME=giss-certificados 
-#export REALM_ADMIN_USER=admin
+#export REALM_ADMIN_USER=$ADMIN_USER
 #export REALM_ADMIN_PASS=CHANGE_ME
 #export PRIVATE_KEY_FILE=/opt/eap/standalone/configuration/privateRsaKey.pem
 #export CERTIFICATE_FILE=/opt/eap/standalone/configuration/caRsaKey.pem
@@ -19,7 +19,7 @@ $JBOSS_HOME/bin/jboss-cli.sh --file=/opt/eap/extensions/actions.cli
 #$JBOSS_HOME/bin/kcadm.sh create components -r $REALM_NAME -s name=rsa-giss -s providerId=rsa -s providerType=org.keycloak.keys.KeyProvider -s 'config.privateKey=["$PRIVATE_KEY_FILE"]’ -s ‘config.certificate=["$CERTIFICATE_FILE"]'
 
 
-#$JBOSS_HOME/bin/kcadm.sh create components -r $REALM_NAME -s name=rsa-giss -s providerId=rsa -s providerType=org.keycloak.keys.KeyProvider -s 'config.privateKey=["/opt/eap/standalone/configuration/privateRsaKey.pem"]' -s 'config.certificate=["/opt/eap/standalone/configuration/caRsaKey.pem"]'
+#$JBOSS_HOME/bin/kcadm.sh create components -r $REALM_NAME -s name=$UN_NOMBRE -s providerId=rsa -s providerType=org.keycloak.keys.KeyProvider -s 'config.privateKey=["/opt/eap/standalone/configuration/privateRsaKey.pem"]' -s 'config.certificate=["/opt/eap/standalone/configuration/caRsaKey.pem"]'
 #echo "Finished kcadm!"
 
 echo "@@@@@@@@@@@@@@@ Finish postconfigure.sh"
